906ccdb3f9e888af36fef1660cb5d4820e2ffe92
supertinypeaunt <1176032017@qq.com>
2020-07-08 10:50:08 +0800

    feat:vue3.0搭建

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,23 @@ const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const RemoveAnnotationPlugin = require('./remove-annotate-plugin')
+const { VueLoaderPlugin }  = require('vue-loader')
+const copyWebpackPlugin  = require('copy-webpack-plugin')
 /**
  * @type {import('webpack').Configuration}
 */
 const config = {
     mode: 'none',
-    entry: './src/index.js',
+    entry: './src/main.js',
     output: {
         path: path.join(__dirname, 'dist')
     },
     module: {
         rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader',
+            },
             {
                 test: /\.md$/,
                 use: './markdown-loader'
@@ -31,11 +37,16 @@ const config = {
         ]
     },
     plugins: [
+        new VueLoaderPlugin(),
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            template: './src/index.html'
+            template: 'public/index.html'
         }),
-        new RemoveAnnotationPlugin()
+
+        new RemoveAnnotationPlugin(),
+        new copyWebpackPlugin({
+            patterns: [{ from: 'public', to: '' }]
+        })
     ]
 }
 
